b215e3206b2c82a7bf274205dad5309afd073d05
Jacob Wegner <jwegner@eldarion.com>
2018-01-17 16:14:09 -0600

    don't try to load json data second time

--- formly/forms/widgets.py
+++ formly/forms/widgets.py
@@ -11,9 +11,6 @@ class MultiTextWidget(MultiWidget):
         kwargs.update({"widgets": widgets})
         super(MultiTextWidget, self).__init__(**kwargs)
 
-    def decompress(self, value):
-        return json.loads(value) if value is not None else []
-
     def format_output(self, rendered_widgets):
         return render_to_string(
             "formly/run/_multiple_input.html",

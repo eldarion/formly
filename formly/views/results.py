1ba0ca9930a147bafaddf43aa2c847afcfb76a39
Jacob Wegner <jwegner@eldarion.com>
2018-01-17 18:06:55 -0600

    decode answer_string kwarg using uri_to_iri

--- formly/views/results.py
+++ formly/views/results.py
@@ -6,6 +6,8 @@ from django.core.exceptions import PermissionDenied
 from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.utils.encoding import uri_to_iri
+from django.utils.functional import cached_property
 from django.views.generic import DetailView
 
 from formly.models import Field, Survey
@@ -29,12 +31,18 @@ class RemapView(LoginRequiredMixin, DetailView):
     context_object_name = "question"
     template_name = "formly/results/remap.html"
 
+    @cached_property
+    def answer_string(self):
+        # decode answer_string, which is usually processed by iri_to_uri
+        # if the url is constructed by Django's default `url` template tag
+        return uri_to_iri(self.kwargs.get("answer_string"))
+
     def get_context_data(self, **kwargs):
         context = super(RemapView, self).get_context_data(**kwargs)
 
         field_results = self.object.results.all()
         context["unmapped_results"] = sorted(create_answer_list(field_results))
-        context["remap_answer"] = unquote(self.kwargs.get("answer_string"))
+        context["remap_answer"] = self.answer_string
         return context
 
     def post(self, request, *args, **kwargs):

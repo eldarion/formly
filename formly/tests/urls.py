a1d8a81bbd25404b1109688bded9bea923ba6771
Graham Ullrich <graham@flyingcracker.com>
2018-02-23 14:15:18 -0700

    Add "home" url for testing

--- formly/tests/urls.py
+++ formly/tests/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    url(r"^home/", TemplateView.as_view(template_name="no-ie.html"), name="home"),
     url(r"^", include("formly.urls", namespace="formly")),
 ]

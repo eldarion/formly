4217bfc7a07acab20351796c6eb74e1dbf2e51bf
Jacob Wegner <jwegner@eldarion.com>
2018-04-12 10:17:38 -0500

    update tests for MultiTextWidget

--- formly/tests/test_forms.py
+++ formly/tests/test_forms.py
@@ -6,4 +6,13 @@ class WidgetTests(SimpleTests):
 
     def test_multi_textwidget_decompress(self):
         widget = MultiTextWidget(widgets_length=2)
-        decompressed = widget.decompress("value")
+
+        # verify that None is decompressed to an empty list
+        value = None
+        decompressed = widget.decompress(value)
+        self.assertEqual(decompressed, [])
+
+        # verify that other values are passed through as-is
+        value = [1, 2]
+        decompressed = widget.decompress(value)
+        self.assertEqual(decompressed, value)
